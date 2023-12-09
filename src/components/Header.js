3f6f0a030df11d31439181a1209227cec5efc6e8
ec369 <erick.calderon@est.fi.uncoma.edu.ar>
2023-12-09 03:11:21 -0300

    prueba

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,8 +6,8 @@ const Header = () => {
   return <header className='py-8'>
   <div className='container mx-auto'> 
     <div className='flex justify-between items-center'>
-      {/* logo */}
-        <a href='#'>
+      {/* logo asd*/}
+        <a href='#'> 
           <img src={Logo} alt='' />
         </a>
         {/* button */}
